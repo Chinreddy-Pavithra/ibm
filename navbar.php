27d96e9c8f050caed16471539429bd39a3ff5526
Pavithra <70309039+Chinreddy-Pavithra@users.noreply.github.com>
2021-05-13 14:42:36 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -5,7 +5,7 @@
 </label>
 <label class="logo">Buddy Bank</label>
   <ul> 
-    <li><a href="bank.php">Home</a></li>
+    <li><a href="index.php">Home</a></li>
 	<li><a href="viewcustomers.php">Make a Transaction</a></li>
 	<li><a href="transactionhistory.php">Transaction History</a></li>
 	<li><a href="loginn.php">Login</a></li>
